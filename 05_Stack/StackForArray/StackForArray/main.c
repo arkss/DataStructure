ef4a1cd6e68b7c4079406df38c45b1a0e1b487c8
arkss <rkdalstjd9@naver.com>
2019-01-10 22:50:20 +0900

    05 스택 배열을 이용한 스택 구현

---
+++ 05_Stack/StackForArray/StackForArray/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+#define MAX_STACK_SIZE 100
+
+typedef int element;
+typedef struct {
+    element stack[MAX_STACK_SIZE];
+    int top;
+} StackType;
+
+// 스택 초기화 함수
+void init(StackType *s)
+{
+    s->top = -1;
+}
+//공백 상태 확인 함수
+int is_empty(StackType *s)
+{
+    return (s->top == -1);
+}
+//포화 상태 확인 함수
+int is_full(StackType *s)
+{
+    return (s->top == (MAX_STACK_SIZE-1));
+}
+// push 함수
+void push(StackType *s, element item)
+{
+    if (is_full(s)){
+        fprintf(stderr, "스택 공백 에러\n");
+        exit(1);
+    }
+    else s->stack[++(s->top)] = item;
+}
+// pop 함수
+element pop(StackType *s)
+{
+    if(is_empty(s)){
+        fprintf(stderr, "스택 공백 에러\n");
+        exit(1);
+    }
+    else return s->stack[(s->top)--];
+}
+
+//peek함수, top에 있는 값만 확인
+element peek(StackType *s)
+{
+    if(is_empty(s)){
+        fprintf(stderr, "스택 공백 에러\n");
+        exit(1);
+    }
+    else return s->stack[(s->top)];
+}
+
+int main(void)
+{
+    StackType s;
+    init(&s);
+    push(&s, 1);
+    push(&s, 2);
+    push(&s, 3);
+    printf("%d\n", pop(&s));
+    printf("%d\n", pop(&s));
+    printf("%d\n", pop(&s));
+    printf("%d\n", is_empty(&s));
+}
